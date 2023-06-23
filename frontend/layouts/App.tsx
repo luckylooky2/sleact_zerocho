c0a0261afdef4e4e04290fcdbe0bacda1498baa8
luckylooky2 <luckylooky2@naver.com>
2023-06-23 23:14:35 +0900

    FEAT : dm page init

--- frontend/layouts/App.tsx
+++ frontend/layouts/App.tsx
@@ -7,6 +7,7 @@ import { Switch, Route, Redirect } from 'react-router';
 const Login = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Channel = loadable(() => import('@pages/Channel')); // workspace layout을 감싼 channel 컴포넌트
+const DirectMessage = loadable(() => import('@pages/DirectMessage')); // workspace layout을 감싼 channel 컴포넌트
 
 // Route : 컴포넌트를 화면에 표시하는 기능
 // Redirect : 다른 페이지로 redirect 시키는 기능(react-router v5) => useNavigate(react-router v6)
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
       <Route path="/workspace/channel" component={Channel} />
+      <Route path="/workspace/dm" component={DirectMessage} />
     </Switch>
   );
 };
---
+++ frontend/pages/DirectMessage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Workspace from '@layouts/Workspace';
+
+const DirectMessage = () => {
+  return (
+    <Workspace>
+      <div>로그인 하신 것을 축하드려요!</div>
+    </Workspace>
+  );
+};
+
+export default DirectMessage;
